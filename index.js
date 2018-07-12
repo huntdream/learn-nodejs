0f183d52f717775c6e195b50358fb0cef99d62ba
maoyu <maoyu960320@hotmail.com>
2018-07-12 12:01:43 +0800

    remove server.listening

--- index.js
+++ index.js
@@ -9,25 +9,21 @@ server.on('request', (req, res) => {
   const ua = req.headers['user-agent']
   console.log(ua)
   res.writeHead(200, {
-    'Connection':'keep-alive',
-    'Content-Length':'hello'.length,
+    'Connection': 'keep-alive',
+    'Content-Length': 'hello'.length,
     'Content-Type': 'text/plain',
-    'Set-Cookie':'username=maoyu'
+    'Set-Cookie': 'username=maoyu'
   })
   console.log(req.url)
   res.write('hello')
   res.end()
 })
 
-server.on('clientError',error=>{
+server.on('clientError', error => {
   console.log(error)
 })
 
-if(!server.listening){
-  server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-  })
-} else  {
-  console.log(`port ${PORT} is in use`)
-}
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
 
