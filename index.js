dbe573dfd8a995eb78e572d6446d0db1c27cef61
maoyu <maoyu960320@hotmail.com>
2018-07-12 11:58:40 +0800

    remove idea config

--- index.js
+++ index.js
@@ -11,14 +11,23 @@ server.on('request', (req, res) => {
   res.writeHead(200, {
     'Connection':'keep-alive',
     'Content-Length':'hello'.length,
-    'Content-Type': 'text/plain'
+    'Content-Type': 'text/plain',
+    'Set-Cookie':'username=maoyu'
   })
   console.log(req.url)
   res.write('hello')
   res.end()
 })
 
-
-server.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
+server.on('clientError',error=>{
+  console.log(error)
 })
+
+if(!server.listening){
+  server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+  })
+} else  {
+  console.log(`port ${PORT} is in use`)
+}
+
