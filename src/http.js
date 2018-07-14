22dd02bcc6383098bda2c8874337496a78ce4d0e
maoyu <maoyu960320@hotmail.com>
2018-07-14 16:17:25 +0800

    update

--- src/http.js
+++ src/http.js
@@ -1,29 +1,66 @@
 const http = require('http')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
+// create a http server
 const server = http.createServer()
 
-
+// listen for request events
 server.on('request', (req, res) => {
-  const ua = req.headers['user-agent']
-  console.log(ua)
+  //get header infomation from request.headers[]
+  let ua = req.headers['user-agent']
+  let username = req.headers['cookie']
+  if (username) {
+    username = username.split('=')[1]
+  } else {
+    username = 'guest'
+  }
+
+  const body =
+    `
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,user-sacle=1">
+    <title>http module</title>
+    </head>
+    <body>
+    <div>
+      <p> Hello ${username},</p> 
+      <p>Your browser's User-Agent is: ${ua},</p>
+      <p>Your IP address is: ${res.socket.remoteAddress}</p>
+      <p>Your source port is: ${res.socket.remotePort}</p>
+    </div>
+    </body>
+    </html>
+
+    `
+
+
+  // set header for response
   res.writeHead(200, {
     'Connection': 'keep-alive',
-    'Content-Length': 'hello'.length,
-    'Content-Type': 'text/plain',
+    'Content-Length': body.length,
+    'Content-Type': 'text/html',
     'Set-Cookie': 'username=maoyu'
   })
-  console.log(req.url)
-  res.write('hello')
+
+  // sends a chunk of response body
+  res.write(body, 'utf-8', () => {
+    console.log('The body has been sent to client')
+  })
+
+  // end current request
   res.end()
 })
 
+// listen for clientError event
 server.on('clientError', error => {
   console.log(error)
 })
 
+// start the server to listen for request
 server.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
+  console.log(`Server started on port http://localhost:${PORT}`)
 })
 
