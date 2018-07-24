dca2375e04165da4dd7636be35d203d6089bae70
maoyu <maoyu960320@hotmail.com>
2018-07-24 20:28:57 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.readdir('./src',(err,dir)=>{
-  if(err) throw  err
-  console.log(dir)
-})
+const DIR = './src'
+
+class Tree {
+  constructor(dir) {
+    this.tree = {}
+  }
+
+  generate(root) {
+    const tree = {}
+    return fs.readdir(root, (err, dirs) => {
+      if (err) throw err
+      dirs.map(dir => {
+        const fullPath = path.resolve(root, dir)
+        fs.stat(fullPath, (err, stats) => {
+          if (err) throw err
+          if (stats.isDirectory()) {
+            fs.readdir(dir, (err, subdirs) => {
+              tree[dir] = { type: 'directory' }
+              tree[dir].children = this.generate(fullPath)
+            })
+          } else if (stats.isFile()) {
+            tree[dir] = { type: 'file' }
+          }
+        })
+      })
+      console.log(tree)
+    })
+  }
+}
+
+const tree = new Tree()
+tree.generate(DIR)
